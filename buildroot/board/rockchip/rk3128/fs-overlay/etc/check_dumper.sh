98dc7de030d691a17810ffe6b9fbfe2c3bea8065
Allen Hao <allen@bte.com.tw>
2021-05-11 15:46:54 +0800

    The way  including some special rom files  in gb.txt, and check_dumper.sh will use Mgba emulator to run them when these rom files are judged to exist. (1) GPSP performance is better, so the default GBA cartridge  are all run with GPSP. (2) But some cartridges  can't  run  well with gpsp, such as GBA   No. 4 Starskyhutch.gba, No. 8 Yugiohdmex06.gba, No. 7 Ougontaiyo_b.gba,  No. 53 Ds_soccer-.gba , YossyNoTamago.gb ,so run with mgba.
    Signed-off-by: Allen Hao <allen@bte.com.tw>

--- buildroot/board/rockchip/rk3128/fs-overlay/etc/check_dumper.sh
+++ buildroot/board/rockchip/rk3128/fs-overlay/etc/check_dumper.sh
@@ -2,8 +2,9 @@ DUMPER_PATH="/media/usb0/"
 ROM_PATH="/userdata/rom/"
 RETROARCH_BIN="/usr/bin/retroarch"
 RETROARCH_CON="/userdata/retroarch/retroarch.cfg"
-GBA_PLUGIN="/usr/lib/libretro/mgba_libretro.so"
+GBA_PLUGIN="/usr/lib/libretro/gpsp_libretro.so"
 GB_PLUGIN="/usr/lib/libretro/vbam_libretro.so"
+MGBA_PLUGIN="/usr/lib/libretro/mgba_libretro.so"
 HOME="/userdata"
 
 if [ ! -f "/tmp/rom.txt" ]; then
@@ -22,6 +23,7 @@ function CheckROM(){
 		fi
 	done
 }
+
 
 function KillGame(){
 	killall retroarch
@@ -58,9 +60,19 @@ function ResetROM(){
 				echo "PREROM=\"${ROM_PATH}${result}\"" > /tmp/rom.txt
 				is_gba=$(echo $result | grep ".gba")
 				if [[ "x$is_gba" != "x" ]]; then
-					HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $GBA_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+					if [ `grep -c  $result /userdata/gb.txt` == 1 ]; then
+						echo "loading:${ROM_PATH}${result} and Execute in Mgba." > /dev/console
+						HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $MGBA_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+					else
+						HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $GBA_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+					fi
 				else
-					HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $GB_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+					if [ `grep -c  $result /userdata/gb.txt` == 1 ]; then
+						echo "loading:${ROM_PATH}${result} and Execute in Mgba." > /dev/console
+						HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $MGBA_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+					else
+						HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $GB_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+					fi
 				fi
 			else
 				echo "No Cartridge" > /dev/console
@@ -95,13 +107,32 @@ else
 	fi
 	/bin/sync
 	result=$(CheckROM)
+	
 	if [[ "x$result" != "x" ]]; then
 		echo "PREROM=\"${ROM_PATH}${result}\"" > /tmp/rom.txt
 		is_gba=$(echo $result | grep ".gba")
 		if [[ "x$is_gba" != "x" ]]; then
-			HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $GBA_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+			if [ `grep -c  $result /userdata/gb.txt` == 1 ]; then
+				echo "loading:${ROM_PATH}${result} and Execute in Mgba." > /dev/console
+				HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $MGBA_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+			else
+				cd /userdata/rom/
+				result=$(CheckROM)
+				new=${result//gba/srm}
+				echo $new	
+				new1=${result//gba}
+				echo $new1
+				ln -s $new $new1"sav"
+				/bin/sync 	
+				HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $GBA_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+			fi
 		else
-			HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $GB_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+			if [ `grep -c  $result /userdata/gb.txt` == 1 ]; then
+				echo "loading:${ROM_PATH}${result} and Execute in Mgba." > /dev/console
+				HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $MGBA_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &				
+			else
+				HOME=$HOME $RETROARCH_BIN -v -c $RETROARCH_CON -L $GB_PLUGIN "${ROM_PATH}${result}" > /dev/console 2>&1 &
+			fi
 		fi
 	fi
 fi
